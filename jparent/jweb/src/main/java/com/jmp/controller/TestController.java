068e6a5835b0870737c96ef42e2dfd89ab75e791
samlai <472023527@qq.com>
2018-07-26 00:37:11 +0800

    接入七牛云的ipip本地ip数据库读取的api

--- jparent/jweb/src/main/java/com/jmp/controller/TestController.java
+++ jparent/jweb/src/main/java/com/jmp/controller/TestController.java
@@ -9,8 +9,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import qiniu.ip17mon.LocationInfo;
+import qiniu.ip17mon.Locator;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -31,5 +35,28 @@ public class TestController {
         LOG.info("test controller.....");
         return objectMapper.writeValueAsString(list);
     }
+
+
+
+//    F:\Code\GitHub\MySsm\jparent\jweb\src\main\resources\17monipdb.datx
+    // test/getIp
+    @RequestMapping(value = {"/getIp"})
+    public String getIp(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        response.setContentType("text/html;charset=utf-8");
+        LOG.info("path : {}",TestController.class.getResource("").toString());
+        LOG.info("path : {}",TestController.class.getResource("/").toString());
+        LOG.info("path : {}",TestController.class.getResource("").getPath());
+        LOG.info("path : {}",TestController.class.getResource("/").getPath());
+        Locator locator = Locator.loadFromLocal(TestController.class.getResource("/").getPath()+"17monipdb.datx");
+        LocationInfo ipInfo = locator.find("180.163.159.7");
+        LOG.info("test ip.....");
+        LOG.info("info country : {}",ipInfo.country);
+        LOG.info("info is : {}",ipInfo.isp);
+        LOG.info("info city : {}",ipInfo.city);
+        return ipInfo.toString();
+    }
+
+
+
 
 }

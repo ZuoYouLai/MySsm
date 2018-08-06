63f6c278321a2a86a3df72bddace1e6ebfbe7964
samlai <472023527@qq.com>
2018-08-06 23:10:26 +0800

    Ascll码的添加

--- jparent/jcomm/src/test/java/SimpleTest.java
+++ jparent/jcomm/src/test/java/SimpleTest.java
@@ -1,6 +1,9 @@
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
+import java.util.Random;
+import java.util.UUID;
+
 public class SimpleTest {
 
 
@@ -12,6 +15,25 @@ public class SimpleTest {
         boolean kflg = StringUtils.isBlank(null);
         System.err.println(kflg);
     }
+    
+    
+     /**
+        *
+        */
+      @Test  
+      public void test002(){
+
+          for(int k=0;k<100;k++) {
+              String str = UUID.randomUUID().toString();
+              System.err.println(str);
+              char end=str.charAt(str.length()-1);
+              byte endNum = (byte) end;
+              System.err.println(end +"  =   "+endNum+"  -->  "+endNum%3);
+
+          }
+
+
+      }
 
 
 

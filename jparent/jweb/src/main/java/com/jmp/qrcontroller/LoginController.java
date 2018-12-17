df62a3ff6e03c712a4b8fb1c950565bc4bfb2d36
samlai <472023527@qq.com>
2018-12-17 12:57:14 +0800

    新表的内容的添加

--- jparent/jweb/src/main/java/com/jmp/qrcontroller/LoginController.java
+++ jparent/jweb/src/main/java/com/jmp/qrcontroller/LoginController.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
